5a70c570a7cda4abcfadf18c243f8b6bf26b4dc1
diusdi <riadidion@gmail.com>
2023-08-30 08:29:07 +0700

    menambahkan file yang telah hilang

---
+++ lib/data/questions.dart
@@ -0,0 +1,55 @@
+import '/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Algoritma dapat dituliskan dalam bentuk?',
+    [
+      'Diagram Flowchart',
+      'Diagram Sequence',
+      'Diagram Usecase',
+      'Diagram Venn',
+    ],
+  ),
+  QuizQuestion('Apa perintah untuk membuat project aplikasi flutter baru?', [
+    'flutter create',
+    'flutter build',
+    'flutter make',
+    'flutter project',
+  ]),
+  QuizQuestion(
+    'Apakah yang dimaksud dengan algoritma?',
+    [
+      'Langkah-langkah sistematis untuk mencapai suatu tujuan',
+      'Cara menuliskan kode program yang baik',
+      'Rumus trigonometri dalam matematika',
+      'Tahapan pembuatan suatu aplikasi',
+    ],
+  ),
+  QuizQuestion(
+    'Apa nama perintah untuk menampilkan informasi software yang dibutuhkan flutter?',
+    [
+      'flutter doctor',
+      'flutter create',
+      'flutter check',
+      'flutter info',
+    ],
+  ),
+  QuizQuestion(
+    'Apa nama perintah untuk menjalankan project aplikasi di device yang tersedia?',
+    [
+      'flutter run',
+      'flutter serve',
+      'flutter serving',
+      'fluter build',
+    ],
+  ),
+  QuizQuestion(
+    'Apa yang dimaksud dengan versioning?',
+    [
+      'Mengatur versi dari source code program',
+      'Mengatur versi dari proses coding',
+      'Mengatur versi dari program tertentu',
+      'Mengatur versi dari aplikasi program',
+    ],
+  ),
+];
